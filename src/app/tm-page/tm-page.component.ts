039cfe9494e43c33f806383f2ca2e813e5ca77e8
yehwenting <yehwenting113@gmail.com>
2018-12-10 17:55:56 +0800

    1210updated

--- src/app/tm-page/tm-page.component.ts
+++ src/app/tm-page/tm-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import a from "../../assets/value.json";
 
 @Component({
   selector: 'app-tm-page',
@@ -8,10 +9,36 @@ import { Router } from '@angular/router';
 })
 export class TMPageComponent implements OnInit {
 
+
   list: string[] = ['CustomerDossiers', 'TemplateDossierStructure', '個人待辦事項', '個人草稿區', '專案資料夾', '技術內容文件', '新北市捷運工程局', '新北市捷運線', '歷史查詢', '測試資料夾', '部門分類', '測試資料夾', '部門分類'];
 
-  constructor() { }
+  tData = JSON.stringify(a);
+  originItem:  Array<string> = a;
+  displayValue: Array<string> = a;
+  title ="技術管理"
+  // data = JSON.stringify(this.tData);
+  
 
+  constructor() { 
+    console.log(a.length);
+    console.log("const: ", this.displayValue);
+
+  };
+
+  getValue(t){
+    console.log("on click: ", t);
+    this.originItem=t;
+    this.displayValue=t.children;
+    console.log("get children: ", this.displayValue);
+    this.title=t.name;
+  }
+
+  back(){
+    console.log("back", this.displayValue);
+    console.log("back origin", this.originItem);
+
+
+  }
   ngOnInit() {
   }
 
