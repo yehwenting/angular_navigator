2b4862492f249ba0295a1c033ac1d6b2b482606b
verahsu860604 <caps4227@gmail.com>
2018-12-10 15:44:58 +0800

    fix positions

--- src/app/tm-page/tm-page.component.ts
+++ src/app/tm-page/tm-page.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class TMPageComponent implements OnInit {
 
-  list: string[] = ['CustomerDossiers', 'TemplateDossierStructure', '個人待辦事項', '個人草稿區', '專案資料夾', '技術內容文件', '新北市捷運工程局', '新北市捷運線', '歷史查詢', '測試資料夾', '部門分類'];
+  list: string[] = ['CustomerDossiers', 'TemplateDossierStructure', '個人待辦事項', '個人草稿區', '專案資料夾', '技術內容文件', '新北市捷運工程局', '新北市捷運線', '歷史查詢', '測試資料夾', '部門分類', '測試資料夾', '部門分類'];
 
   constructor() { }
 
